7628f9473f36e0a74df010e8dd3a3f97df033d69
AdrianFullStack <rogelio.ast@gmail.com>
2020-06-03 04:49:41 -0500

    update test

--- src/test/persona.test.js
+++ src/test/persona.test.js
@@ -1,11 +1,19 @@
-//require('../util/TestConfigurador').cargarVariablesEntorno();
 const FuncPersona = require('../functions/persona');
 
 const events = {
-    pathParameters : {operacion : 'consultanotificacion'},
-    resource: 'personas/{id}',
-    httpMethod : 'GET',
+    //pathParameters : {id : 1},
+    //resource: 'personas/{id}',
+    httpMethod : 'POST',
     body : JSON.stringify({
+        nombre: 'Luke Skywalker',
+        altura: '172',
+        masa: '77',
+        genero: 'male',
+        anio_de_nacimiento: '19BBY',
+        color_de_ojos: 'blue',
+        color_de_piel: 'fair',
+        color_de_pelo: 'blond',
+        mundo_natal: 'https://swapi.py4e.com/api/planets/1/'
     })
 };
 
